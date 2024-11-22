a446b3df74cfec388479451da56fcd7a1e815cc8
Robert Baufeld <rbaufeld@posteo.de>
2024-11-22 13:53:15 +0100

    set inital markers for coordinates from coordinates.ts

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -15,6 +15,10 @@ import { fromLonLat } from 'ol/proj';
 import coordinates from "../../coordinates.ts"
 
 onMounted(() => {
+  initiateMap();
+});
+
+function initiateMap() {
   const map = new Map({
     target: 'map', // ID of the map container
     layers: [
@@ -23,41 +27,55 @@ onMounted(() => {
       }),
     ],
     view: new View({
-      center: fromLonLat([13.405, 52.52]), // Coordinates for Berlin
-      zoom: 9,
+      center: fromLonLat([13.4900, 52.4672]), // Coordinates for Berlin 1501659.13533162, 6885117.554255853
+      zoom: 16,
     }),
   });
-  // source to store pin data
-  const vectorSource = new VectorSource();
 
-  // vector layer to display pins
-  const vectorLayer = new VectorLayer({
-    source: vectorSource,
-  });
-  map.addLayer(vectorLayer);
+  createPinsFromList(map);
+}
 
-  // create a pin
-  const pinStyle = new Style({
-    image: new Icon({
-      anchor: [0.5, 1],
-      src: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // Marker icon
-      scale: 0.1,
-    })
+const vectorSource = new VectorSource();
+// vector layer to display pins
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+});
+
+// create a pin
+const pinStyle = new Style({
+  image: new Icon({
+    anchor: [0.5, 1],
+    src: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // Marker icon
+    scale: 0.1,
   })
+})
 
-  map.on("click", (event) => {
-    const coordinate = event.coordinate;
+// function createPinOnClick(map: Map) {
+//   // source to store pin data
+//   map.addLayer(vectorLayer);
+//   map.on("click", (event) => {
+//     const coordinate = event.coordinate;
+//     console.log(coordinate);
+//     const pin = new Feature({
+//       geometry: new Point(coordinate),
+//     })
+//     pin.setStyle(pinStyle);
+//     vectorSource.addFeature(pin);
+//     console.log('Pin added at:', coordinate);
+//   })
+// }
+
+function createPinsFromList(map: Map) {
+  map.addLayer(vectorLayer);
+  coordinates.forEach((coord) => {
     const pin = new Feature({
-      geometry: new Point(coordinate),
+      geometry: new Point(coord.longlat)
     })
     pin.setStyle(pinStyle);
     vectorSource.addFeature(pin);
-    console.log('Pin added at:', coordinate);
+    console.log('Pin added at:', coord.longlat);
   })
-
-
-});
-
+}
 </script>
 
 <template>
