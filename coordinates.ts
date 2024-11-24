ac8309fe180862351380936793687dca94c85dec
Robert Baufeld <rbaufeld@posteo.de>
2024-11-24 16:50:45 +0100

    changed coordinate data

--- coordinates.ts
+++ coordinates.ts
@@ -1,16 +1,28 @@
 interface Coordinate {
   name: string,
+  description: string,
+  address: string,
+  postalCode: string
   longlat: number[]
 }
 
 const coordinates: Coordinate[] = [{
   name: "Zu Hause",
+  description: "Hier wohne ich",
+  address: "Ludwig-Klapp-Straße 3",
+  postalCode: "12437 Berlin",
   longlat: [1501430.2213757674, 6885113.958340392],
 },{
-  name: "Bücherladen",
+  name: "Büchereck ",
+  description: "lokaler Bücherladen",
+  address: "Ekkehardstraße 1",
+  postalCode: "12437 Berlin",
   longlat: [1501551.186631467, 6885071.834759387],
 },{
   name: "Bahnhof Baumschulenweg",
+  description: "S-Bahnhof",
+  address: "Baumschulenstraße",
+  postalCode: "12437 Berlin",
   longlat: [1501659.13533162, 6885117.554255853],
 },
 ]
